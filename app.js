31fe536b58a545034b543768064e09d2382bf39a
Anton-Yau <49604087+Anton-Yau@users.noreply.github.com>
2019-04-20 16:49:36 -0400

    Added Chart, basically done

--- app.js
+++ app.js
@@ -1,18 +1,15 @@
 //Provide Account Balance
 document.getElementById("search").onclick = function() {
   let userInput = document.getElementById("basic-url").value;
-  console.log(userInput);
-  let api = `https://api.blockcypher.com/v1/btc/test3/addrs/${userInput}/balance`;
+  let api = `https://api.blockcypher.com/v1/btc/test3/addrs/${userInput}`;
   fetch(api)
     .then(response => {
       if (!response.ok) {
         throw Error(response.statusText);
       }
-      console.log(response);
       return response.json();
     })
     .then(data => {
-      console.log(data);
       const balance = data.balance / 10 ** 8;
       const unconfirmedBalance = data.unconfirmed_balance / 10 ** 8;
       const finalBalance = data.final_balance / 10 ** 8;
@@ -25,6 +22,16 @@ document.getElementById("search").onclick = function() {
       document.getElementById(
         "showFinalBalance"
       ).innerHTML = `Final Balance ฿${finalBalance}`;
+      let txList = [];
+      for (tx of data.txrefs) {
+        let x = tx.block_height;
+        let y = tx.ref_balance / 10 ** 8;
+        let point = { x, y };
+        txList.unshift(point);
+      }
+      console.log(txList);
+      drawChart(txList);
+      document.getElementById("info").style.display = "block";
     })
     .catch(function(error) {
       alert("Cannot find this address, please check again.");
@@ -40,7 +47,6 @@ document.getElementById("transfer").onclick = function() {
     inputs: [{ addresses: [from] }],
     outputs: [{ addresses: [to], value: v }]
   };
-  console.log(JSON.stringify(newtx));
   fetch("https://api.blockcypher.com/v1/btc/test3/txs/new", {
     method: "post",
     body: JSON.stringify(newtx)
@@ -68,7 +74,6 @@ document.getElementById("transfer").onclick = function() {
       });
       Promise.all(promises).then(result => {
         tmptx.signatures = result;
-        console.log("tmptx " + JSON.stringify(tmptx));
         fetch("https://api.blockcypher.com/v1/btc/test3/txs/send", {
           method: "post",
           body: JSON.stringify(tmptx)
@@ -87,3 +92,53 @@ document.getElementById("transfer").onclick = function() {
       alert(error + ", make sure to enter the corrrect input.");
     });
 };
+function drawChart(d) {
+  let Chart = require("chart.js");
+  let ctx = document.getElementById("myChart").getContext("2d");
+  let lineChart = new Chart(ctx, {
+    type: "line",
+    data: {
+      datasets: [
+        {
+          label: "Account Balance(฿)",
+
+          data: d,
+          backgroundColor: "rgba(0,0,255,0.9)",
+          borderColor: "rgba(0,0,255,0.5)",
+          fill: false
+        }
+      ]
+    },
+    options: {
+      title: {
+        display: true,
+        text: "Account Balance Over Block Height",
+        fontColor: "white"
+      },
+      legend: {
+        labels: {
+          fontColor: "white"
+        }
+      },
+      scales: {
+        yAxes: [
+          {
+            ticks: {
+              fontColor: "white"
+            }
+          }
+        ],
+        xAxes: [
+          {
+            label: "Block Height",
+            type: "linear",
+            position: "bottom",
+            ticks: {
+              fontColor: "white"
+            }
+          }
+        ]
+      }
+    }
+  });
+}
